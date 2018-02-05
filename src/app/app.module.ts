66feca5550f92815908c99e1462eb172de44c650
Sirito-Olivier <roger.sirito-olivier@etu.univ-grenoble-alpes.fr>
2018-02-06 00:06:45 +0100

    lolololol

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AproposComponent } from './Apropos/apropos.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AproposComponent
   ],
   imports: [
     BrowserModule
